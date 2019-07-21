0ebeeb760aa26c406de83d6a3ee9966f67a96c24
Jim Nusbaum <raymond.nusbaum@getyourvi.com>
2019-07-21 14:32:02 -0600

    Squashed 'vimailserver/' changes from 8e4591a..9859bdb
    9859bdb script to start server
    31956a2 script to start server
    fe8f56a script to start server
    
    git-subtree-dir: vimailserver
    git-subtree-split: 9859bdba09f8db1ef15aecfe0266f327f99539e4

--- start_server.sh
+++ start_server.sh
@@ -7,4 +7,4 @@ MAILSVR="smtp.gmail.com"; export MAILSVR
 MAILUSER="vicalc@getyourvi.com"; export MAILUSER
 REDISBROKER='redis+socket://var/run/redis/redis-server.sock'; export REDISBROKER
 VIMSPATH="/home/viadm/VIINC/vimailserver"; export VIMSPATH
-$VIMSPATH/venv/bin/celery -A mail_server worker --loglevel=debug >> $VIMSPATH/log/mail_server.log 2>&1 &
+$VIMSPATH/venv/bin/celery -A mail_server.celery worker --loglevel=debug >> $VIMSPATH/log/mail_server.log 2>&1 &
