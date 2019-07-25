acc930c1e91714b71af52f53dcb3e579696f64fc
Jim Nusbaum <raymond.nusbaum@getyourvi.com>
2019-07-25 16:09:43 -0600

    Squashed 'vimailserver/' changes from e1fa37a..ef67831
    ef67831 script to start server
    
    git-subtree-dir: vimailserver
    git-subtree-split: ef678317486e564152b2e362382ab82c134605c0

--- mail_tasks.py
+++ mail_tasks.py
@@ -1,48 +1,10 @@
-import os
 import logging
 from email.message import EmailMessage
 from typing import Dict
 import smtplib
-
-from celery import Celery
-from msrestazure.azure_active_directory import MSIAuthentication
-from azure.keyvault.key_vault_client import KeyVaultClient
 from vidb.models import *
 
-dbhost = os.getenv('DBHOST')
-database = os.getenv('DATABASE')
-dbuser = os.getenv('DBUSER')
-dbsslmode = os.getenv('DBSSLMODE')
-
-# Create MSI Authentication
-credentials = MSIAuthentication(resource='https://vault.azure.net')
-key_vault_client = KeyVaultClient(credentials)
-key_vault_uri = 'https://viinc.vault.azure.net'
-# get database password from key vault
-secret = key_vault_client.get_secret(key_vault_uri, "BACKEND-DB-PWD", "")
-dbpwd = secret.value
-
-print("connecting to %s:%s:%s" % (dbhost, database, dbuser))
-
-# configure from environment variables
-# these will come in secure form from azure app settings in azure deployment
-# database/pony
-db.bind(provider='postgres', host=dbhost,
-        database=database,
-        user=dbuser,
-        password=dbpwd,
-        sslmode=dbsslmode)
-db.generate_mapping()
-
-mailsvr = os.getenv('MAILSVR')
-mailuser = os.getenv('MAILUSER')
-# get mail password from key vault
-secret = key_vault_client.get_secret(key_vault_uri, "MAILPWD", "")
-mailpwd = secret.value
-
-
-celery = Celery('mail_server')
-celery.config_from_object('celeryconfig')
+from celeryconfig import celery, mailsvr, mailuser, mailpwd
 
 
 def build_reminder_mail(name: str, index_id: str, counts: Dict[str, Dict[str, int]]) -> EmailMessage:
