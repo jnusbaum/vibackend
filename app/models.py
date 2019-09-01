f12314d848986fbb5359970faacfe431c9fe1222
Jim Nusbaum <raymond.nusbaum@getyourvi.com>
2019-08-31 19:01:56 -0600

    port to SQLAlchemy and major restructuring

--- app/models.py
+++ app/models.py
@@ -14,11 +14,12 @@ class User(db.Model):
     role = db.Column(db.Text, nullable=False, default='viuser')  # one of vivendor, viuser
     last_login = db.Column(db.DateTime, index=True)
     last_notification = db.Column(db.DateTime, index=True)
+    # foreign keys
     # relationships
     results = db.relationship('Result', back_populates='user')
     answers = db.relationship('Answer', back_populates='user')
     tokens = db.relationship('Token', back_populates='user')
-
+    # indexes
     db.composite_index(email, pword)
 
 
@@ -29,8 +30,9 @@ class Token(db.Model):
     token_type = db.Column(db.Text, nullable=False)
     revoked = db.Column(db.Boolean, nullable=False)
     expires = db.Column(db.DateTime, nullable=False)
-    # relationships
+    # foreign keys
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False, index=True)
+    # relationships
     user = db.relationship('User', back_populates='tokens')
 
 
@@ -47,6 +49,7 @@ class Question(db.Model):
     name = db.Column(db.Text, primary_key=True)
     info = db.Column(db.Text, nullable=False, default='not yet')
     qtext = db.Column(db.Text, nullable=False, default='not yet')
+    # foreign keys
     # relationships
     index_sub_components = db.relationship('IndexSubComponent', secondary=indexsubcomponent_question, back_populates='questions')
     answers = db.relationship('Answer', back_populates='question')
@@ -68,11 +71,13 @@ class Answer(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     time_received = db.Column(db.DateTime, nullable=False)
     answer = db.Column(db.Text, nullable=False)
+    # foreign keys
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False, index=True)
     question_name = db.Column(db.Text, db.ForeignKey('question.name'), nullable=False, index=True)
     # relationships
     user = db.relationship('User', back_populates='answers')
     question = db.relationship('Question', back_populates='answers')
+    results = db.relationship('Result', secondary=answer_result, back_populates='answers')
     # indexes
     db.composite_index(user_id, question_name, time_received)
 
@@ -87,6 +92,7 @@ class Result(db.Model):
     time_generated = db.Column(db.DateTime, nullable=False)
     points = db.Column(db.Integer, nullable=False)
     maxforanswered = db.Column(db.Integer, nullable=False)
+    # foreign keys
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False, index=True)
     index_name = db.Column(db.Text, db.ForeignKey('index.name'), nullable=False, index=True)
     # relationships
@@ -94,7 +100,7 @@ class Result(db.Model):
     index = db.relationship('Index', back_populates='results')
     answers = db.relationship('Answer', secondary=answer_result, back_populates='results')
     result_components = db.relationship('ResultComponent', back_populates='result')
-
+    # indexes
     db.composite_index(user_id, index_name, time_generated)
 
     @hybrid_property
@@ -111,11 +117,13 @@ class ResultComponent(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     points = db.Column(db.Integer)
     maxforanswered = db.Column(db.Integer)
-    # relationships
+    # foreign keys
     result_id = db.Column(db.Integer, db.ForeignKey('result.id'), nullable=False, index=True)
     indexcomponent_name = db.Column(db.Text, db.ForeignKey('indexcomponent.name'), nullable=False, index=True)
-
-    result_sub_components = db.relationship('ResultSubComponent', back_populates='result_components')
+    # relationships
+    result = db.relationship('Result', back_populates='result_components')
+    result_sub_components = db.relationship('ResultSubComponent', back_populates='result_component')
+    index_component = db.relationship('IndexComponent', back_populates='result_components')
 
     @hybrid_property
     def name(self):
@@ -131,12 +139,12 @@ class ResultSubComponent(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     points = db.Column(db.Integer)
     maxforanswered = db.Column(db.Integer)
-    # relationships
+    # foreign keys
     resultcomponent_id = db.Column(db.Integer, db.ForeignKey('resultcomponent.id'), nullable=False, index=True)
     indexsubcomponent_name = db.Column(db.Text, db.ForeignKey('indexsubcomponent.name'), nullable=False, index=True)
-
-    result_component = db.Column(ResultComponent)
-    index_sub_component = db.Column('IndexSubComponent')
+    # relationships
+    result_component = db.relationship('ResultComponent', back_populates='result_sub_components')
+    db.relationship('IndexSubComponent', secondary=indexsubcomponent_question, back_populates='questions')
 
     @hybrid_property
     def name(self):
@@ -151,10 +159,10 @@ class Index(db.Model):
     __tablename__ = 'index'
     name = db.Column(db.Text, primary_key=True)
     maxpoints = db.Column(db.Integer)
+    # foreign keys
     # relationships
-
-    index_components = db.relationship('IndexComponent')
-    results = db.relationship(Result)
+    index_components = db.relationship('IndexComponent', back_populates='index')
+    results = db.relationship('Result', back_populates='index')
 
     @hybrid_property
     def questions(self):
@@ -167,10 +175,11 @@ class IndexComponent(db.Model):
     maxpoints = db.Column(db.Integer)
     info = db.Column(db.Text)
     recommendation = db.Column(db.Text)
-    # relationships
+    # foreign keys
     index_name = db.Column(db.Text, db.ForeignKey('index.name'), nullable=False, index=True)
-    index_sub_components = db.relationship('IndexSubComponent')
-    result_components = db.relationship(ResultComponent)
+    # relationships
+    index_sub_components = db.relationship('IndexSubComponent', back_populates='index_component')
+    result_components = db.relationship('ResultComponent', back_populates='index_component')
 
     @hybrid_property
     def questions(self):
@@ -183,10 +192,10 @@ class IndexSubComponent(db.Model):
     maxpoints = db.Column(db.Integer)
     info = db.Column(db.Text)
     recommendation = db.Column(db.Text)
-    # relationships
+    # foreign keys
     indexcomponent_name = db.Column(db.Text, db.ForeignKey('indexcomponent.name'), nullable=False, index=True)
-    index_component = db.relationship('IndexComponent')
+    # relationships
+    questions = db.relationship('Question', secondary=indexsubcomponent_question, back_populates='index_sub_components')
+    index_component = db.relationship('IndexComponent', back_populates='index_sub_components')
+    result_sub_components = db.relationship('ResultSubComponent', back_populates='index_sub_component')
 
-    result_sub_components = db.relationship(ResultSubComponent)
-    result_sub_components = db.relationship(ResultSubComponent)
-    questions = db.relationship(Question)
