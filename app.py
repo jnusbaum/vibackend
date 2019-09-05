fb80176e121a9c44f655bc181ada37445225e418
Jim Nusbaum <raymond.nusbaum@getyourvi.com>
2019-09-04 19:37:51 -0600

    comments

--- app.py
+++ app.py
@@ -10,15 +10,15 @@ from datetime import datetime
 # set up logger
 # app.logger.setLevel(app.config['LOGLEVEL'])
 logfile = app.config['LOGDIR'] + app.config['LOGNAME']
-# check for existence and rotate
-if os.path.isfile(logfile):
-    # rename with time
-    bname = app.config['LOGNAME'].split('.')[0]
-    nname = "%s.%s.log" % (bname, datetime.utcnow().strftime("%Y-%m-%d-%H-%M-%S"))
-    os.rename(logfile, os.path.join(app.config['LOGDIR'], nname))
-else:
-    # create log directory if it does not exist
-    os.makedirs(app.config['LOGDIR'], 0o777, True)
+# # check for existence and rotate
+# if os.path.isfile(logfile):
+#     # rename with time
+#     bname = app.config['LOGNAME'].split('.')[0]
+#     nname = "%s.%s.log" % (bname, datetime.utcnow().strftime("%Y-%m-%d-%H-%M-%S"))
+#     os.rename(logfile, os.path.join(app.config['LOGDIR'], nname))
+# else:
+#     # create log directory if it does not exist
+#     os.makedirs(app.config['LOGDIR'], 0o777, True)
 import logging
 # fh = logging.FileHandler(logfile)
 # fh.setLevel(app.config['LOGLEVEL'])
