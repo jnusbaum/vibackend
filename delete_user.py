59cb529ecdf09af8f128cbc59a1fd8e2ac0dd386
Jim Nusbaum <raymond.nusbaum@getyourvi.com>
2019-09-25 10:00:51 -0600

    Squashed 'vidb/' changes from 754c85e..b36e619
    b36e619 sqlalchemy port
    37e661c sqlalchemy port
    9269ab5 sqlalchemy port
    4d106a5 sqlalchemy port
    e1389a6 sqlalchemy port
    378136a sqlalchemy port
    2a8a986 sqlalchemy port
    
    git-subtree-dir: vidb
    git-subtree-split: b36e619874fcf42063a2de573df442add4cb183c

--- delete_user.py
+++ delete_user.py
@@ -9,18 +9,13 @@ from sqlalchemy.orm import sessionmaker
 
 from models import *
 
-DBHOST = os.environ.get('DBHOST') or '192.168.0.134'
-DATABASE = os.environ.get('DATABASE') or 'vibackend'
-DBUSER = os.environ.get('DBUSER') or 'vi@viback'
-DBPWD = os.environ.get('DBPWD') or 'T3QXCejm7GnCZtTH'
-# encode password
-DBPWD = parse.quote_plus(DBPWD)
-
-
 dbhost = os.environ.get('DBHOST') or '192.168.0.134'
-database = os.environ.get('DATABASE') or 'vibackend'
+database = os.environ.get('DATABASE') or 'vibackend-test'
 dbuser = os.environ.get('DBUSER') or 'vi@viback'
-dbpwd = os.environ.get('DBPWD')
+dbpwd = os.environ.get('DBPWD') or 'foobar'
+dbdriver = '{ODBC Driver 17 for SQL Server}'
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 if not dbpwd:
     # Create MSI Authentication
@@ -31,11 +26,11 @@ if not dbpwd:
                                          "BACKEND-DB-PWD",  # Name of your secret
                                          "")  # The version of the secret. Empty string for latest
     dbpwd = secret.value
-dbpwd = parse.quote_plus(dbpwd)
-SQLALCHEMY_DATABASE_URI = 'mssql+pymssql://{user}:{password}@{host}/{db}?charset=utf8'.format(user=dbuser,
-                                                                                              password=dbpwd,
-                                                                                              host=dbhost,
-                                                                                              db=database)
+cstring = 'Driver={driver};Server=tcp:{dbhost},1433;Database={database};Uid={dbuser};Pwd={dbpwd};Encrypt=yes;TrustServerCertificate=yes;Connection Timeout=30;'
+cstring = cstring.format(driver=dbdriver, dbhost=dbhost, database=database, dbuser=dbuser, dbpwd=dbpwd)
+cstring = parse.quote_plus(cstring)
+SQLALCHEMY_DATABASE_URI = "mssql+pyodbc:///?odbc_connect=%s" % cstring
+
 engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -43,7 +38,7 @@ session = Session()
 print("connecting to %s:%s:%s" % (dbhost, database, dbuser))
 
 # ERASE THIS AFTER USING!!!
-email = 'webmaster@vitalityindex.com'
+email = 'raymond.nusbaum@gmail.com'
 
 u = session.query(User).filter(User.email == email).one_or_none()
 if u:
